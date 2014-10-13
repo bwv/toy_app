a977e7343ead3e549dc425c5bf057fa9db2d1504
Niels <nielsjhdr@gmail.com>
2014-10-13 19:14:13 +0200

    Added goodbye

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#hello'
 
+  get '/goodbye', to: 'application#goodbye'
+
+
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
